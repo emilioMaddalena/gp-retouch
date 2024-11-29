c86c31690b2549e3159d545fe36e9c12aa4b9d1d
emilioMaddalena <emilio.tanowe.maddalena@gmail.com>
2024-11-29 21:57:21 +0100

    Extend ImageProcessor
    - tests are missing

--- gp_retouch/image/image_processor.py
+++ gp_retouch/image/image_processor.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numpy as np
 from skimage.transform import resize
 
@@ -15,8 +16,11 @@ class ImageProcessor:
         Returns:
             np.ndarray: _description_
         """
-        new_shape = (int(data.shape[0] * factor), int(data.shape[1] * factor))
-        return resize(data, new_shape, anti_aliasing=True)
+        if len(data.shape) == 2:
+            new_shape = (int(data.shape[0] * factor), int(data.shape[1] * factor))
+            return resize(data, new_shape, anti_aliasing=True)
+        else:
+            raise ValueError("Only grayscale images are accepted.")
 
     def convert_to_grayscale(self, data: np.ndarray) -> np.ndarray:
         """_summary_.
@@ -30,3 +34,24 @@ class ImageProcessor:
         if len(data.shape) == 3 and data.shape[2] == 3:  # RGB image
             return np.mean(data, axis=2).astype(np.uint8)
         return data  # Already grayscale
+
+    def convert_to_rgb(self, data: np.ndarray) -> np.ndarray:
+        """_summary_.
+
+        Args:
+            data (np.ndarray): _description_
+
+        Returns:
+            np.ndarray: _description_
+        """
+        pass
+
+    def print(self, data: np.ndarray):
+        """_summary_.
+
+        Args:
+            data (np.ndarray): _description_
+        """
+        plt.imshow(data, cmap="gray")  # Use 'gray' for grayscale images
+        plt.axis("off")  # Turn off axis labels and ticks
+        plt.show()
