c0fcbb95330ff9582ff949f80206cd9acc71a8c0
emilioMaddalena <emilio.tanowe.maddalena@gmail.com>
2024-12-06 07:32:34 +0100

    Fix plot method
    - plot was interpreting gray images as RGB ones
    - Now it can plot each in the right way

--- gp_retouch/image/image.py
+++ gp_retouch/image/image.py
@@ -50,6 +50,9 @@ class Image:
         pass
 
     def plot(self):  # noqa: D102
-        plt.imshow(self.data)
-        plt.axis("off")  # Turn off the axis for better visualization
+        if self.is_rgb:
+            plt.imshow(self.data)
+        else:
+            plt.imshow(self.data, cmap='gray', vmin=0, vmax=255)
+        plt.axis("off")  
         plt.show()
