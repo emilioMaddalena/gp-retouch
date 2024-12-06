3b88bf0482f30ebd4333d32c8d4f881fab253154
emilioMaddalena <emilio.tanowe.maddalena@gmail.com>
2024-12-06 07:31:22 +0100

    Introduce support for different image types
    - load_image can now correctly distinguish RGB and grayscale images, and extracts the right data from them

--- gp_retouch/loader.py
+++ gp_retouch/loader.py
@@ -6,16 +6,48 @@ from PIL import Image as PILImage
 from .image.image import Image
 
 
-def load_image(path_str: str, metadata: dict) -> Image:
-    """_summary_.
+def load_image(path_str: str | Path, metadata: dict = {}) -> Image:
+    """Load the target image.
+
+    Returns an Image object with the appropriate data in it.
+    Additional logic is embedded here to detect grayscale images
+    that were saved as RGB are recognized as grayscale.
 
     Args:
-        path_str (str): _description_
-        metadata (dict): _description_
+        path_str (str | Path): path to the image.
+        metadata (dict): any extra info to be attached to it.
 
     Returns:
-        Image: _description_
+        Image: the final Image object.
     """
     image = PILImage.open(path_str)
-    image_data = np.array(image.convert("RGB"))
+    if image.mode == "L":  # grayscale
+        image_data = np.array(image)
+    elif image.mode in ["RGB", "RGBA"]:
+        image_data = np.array(image.convert("RGB"))
+        # Check if it was saved as RGB but actually is gray
+        if has_identical_channels(image_data):
+            image_data = image_data[:, :, 0]
+    else:
+        raise ValueError(f"Unsupported image mode: {image.mode}")
     return Image(data=image_data, metadata=metadata)
+
+
+def has_identical_channels(image_data: np.ndarray) -> bool:
+    """Check if an RGB is faking it.
+
+    Verifies if all three channels of an RGB image has the same
+    information. That is, if it's in reality a grayscale image.
+
+    Args:
+        image_data (np.ndarray): the image to check.
+
+    Returns:
+        bool: if all its channels contain the same array.
+    """
+    if np.all(image_data[:, :, 0] == image_data[:, :, 1]) & np.all(
+        image_data[:, :, 1] == image_data[:, :, 2]
+    ):
+        return True
+    else:
+        return False
